8ee8ca1d7827bb8e8417965ee890e7155563ea90
szumaster3 <szumaster2009@protonmail.com>
2026-01-11 10:38:55 +0100

    Auto generate journal

---
+++ data/screenshots/s_singleplayer/generate_journal.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+SCREENSHOT_DIR="."
+OUTPUT_FILE="$SCREENSHOT_DIR/journal.json"
+
+# Tworzymy pusty JSON
+echo "[" > "$OUTPUT_FILE"
+
+files=($(ls "$SCREENSHOT_DIR"/*.png | sort))
+
+declare -A dates
+
+for f in "${files[@]}"; do
+    base=$(basename "$f")
+    date="${base:0:10}"
+
+    if [[ -z "${dates[$date]}" ]]; then
+        dates[$date]=""
+    fi
+    if [[ -z "${dates[$date]}" ]]; then
+        dates[$date]="$base"
+    else
+        dates[$date]="${dates[$date]},$base"
+    fi
+done
+
+first=true
+for date in $(printf "%s\n" "${!dates[@]}" | sort); do
+    images="${dates[$date]}"
+    IFS=',' read -ra arr <<< "$images"
+
+    thumb="${arr[0]}"
+
+    if [ "$first" = true ]; then
+        first=false
+    else
+        echo "," >> "$OUTPUT_FILE"
+    fi
+
+    echo "  {" >> "$OUTPUT_FILE"
+    echo "    \"date\": \"$date\"," >> "$OUTPUT_FILE"
+    echo "    \"thumb\": \"$thumb\"," >> "$OUTPUT_FILE"
+    echo "    \"images\": [" >> "$OUTPUT_FILE"
+
+    for i in "${!arr[@]}"; do
+        comma=","
+        if [ $i -eq $((${#arr[@]} - 1)) ]; then
+            comma=""
+        fi
+        echo "      \"${arr[$i]}\"$comma" >> "$OUTPUT_FILE"
+    done
+
+    echo "    ]" >> "$OUTPUT_FILE"
+    echo -n "  }" >> "$OUTPUT_FILE"
+done
+
+echo "" >> "$OUTPUT_FILE"
+echo "]" >> "$OUTPUT_FILE"
+
+echo "journal.json created $OUTPUT_FILE"
